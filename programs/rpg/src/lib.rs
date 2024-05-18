e61da2698a39762d01d9befbf7da70e32bb28994
ASCorreia <36857753+ASCorreia@users.noreply.github.com>
2024-05-18 16:35:06 +0100

    Updated to anchor 0.30 and solana 1.18
    - Updated to anchor version 0.30
    
    - Updated to solana program 1.18
    
    - Major changes are being performed

--- programs/rpg/src/lib.rs
+++ programs/rpg/src/lib.rs
@@ -9,9 +9,7 @@ declare_id!("2jqUJr2sQzfNhzpTEnUnMbM2enCs4kQG6YTjmm1SZ9rN");
 pub struct Game { // 8 bytes
     pub game_master: Pubkey,            // 32 bytes
     pub treasury: Pubkey,               // 32 bytes
-
-    pub action_points_collected: u64,   // 8 bytes
-    
+    pub action_points_collected: u64,   // 8 bytes  
     pub game_config: GameConfig,
 }
 
@@ -19,17 +17,13 @@ pub struct Game { // 8 bytes
 pub struct Player { // 8 bytes
     pub player: Pubkey,                 // 32 bytes
     pub game: Pubkey,                   // 32 bytes
-
     pub action_points_spent: u64,               // 8 bytes
     pub action_points_to_be_collected: u64,     // 8 bytes
-
     pub status_flag: u8,                // 8 bytes
     pub experience: u64,                 // 8 bytes
     pub kills: u64,                     // 8 bytes
     pub next_monster_index: u64,        // 8 bytes
-
     pub for_future_use: [u8; 256],      // Attack/Speed/Defense/Health/Mana?? Metadata??
-
     pub inventory: Vec<InventoryItem>,  // Max 8 items
 }
 
@@ -37,7 +31,6 @@ pub struct Player { // 8 bytes
 pub struct Monster { // 8 bytes
     pub player: Pubkey,                 // 32 bytes
     pub game: Pubkey,                   // 32 bytes
-
     pub hitpoints: u64,                 // 8 bytes
 }
 
@@ -87,7 +80,8 @@ pub fn spend_action_points<'info>(
         Transfer {
             from: player.clone(),
             to: player_account.to_account_info().clone(),
-        });
+        }
+    );
     transfer(cpi_context, action_points)?;
 
     msg!("Minus {} action points", action_points);
@@ -107,11 +101,8 @@ pub struct CreateGame<'info> {
         space = std::mem::size_of::<Game>()+ 8
     )]
     pub game: Account<'info, Game>,
-
     #[account(mut)]
     pub game_master: Signer<'info>,
-
-    /// CHECK: Need to know they own the treasury
     pub treasury: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
@@ -133,7 +124,6 @@ pub fn run_create_game(ctx: Context<CreateGame>, max_items_per_player: u8) -> Re
 #[derive(Accounts)]
 pub struct CreatePlayer<'info> {
     pub game: Box<Account<'info, Game>>,
-
     #[account(
         init, 
         seeds=[
@@ -146,10 +136,8 @@ pub struct CreatePlayer<'info> {
         space = std::mem::size_of::<Player>() + std::mem::size_of::<InventoryItem>() * game.game_config.max_items_per_player as usize + 8)
     ]
     pub player_account: Account<'info, Player>,
-
     #[account(mut)]
     pub player: Signer<'info>,
-
     pub system_program: Program<'info, System>,
 }
 
@@ -164,16 +152,15 @@ pub fn run_create_player(ctx: Context<CreatePlayer>) -> Result<()> {
 
     msg!("Hero has entered the game!");
 
-    {   // Spend 100 lamports to create player
-        let action_points_to_spend = 100;
+    // Spend 100 lamports to create player
+    let action_points_to_spend = 100;
 
-        spend_action_points(
-            action_points_to_spend, 
-            &mut ctx.accounts.player_account,
-            &ctx.accounts.player.to_account_info(), 
-            &ctx.accounts.system_program.to_account_info()
-        )?;
-    }
+    spend_action_points(
+        action_points_to_spend, 
+        &mut ctx.accounts.player_account,
+        &ctx.accounts.player.to_account_info(), 
+        &ctx.accounts.system_program.to_account_info()
+    )?;
 
     Ok(())
 }
@@ -182,13 +169,11 @@ pub fn run_create_player(ctx: Context<CreatePlayer>) -> Result<()> {
 #[derive(Accounts)]
 pub struct SpawnMonster<'info> {
     pub game: Box<Account<'info, Game>>,
-
     #[account(mut,
         has_one = game,
         has_one = player,
     )]
     pub player_account: Box<Account<'info, Player>>,
-
     #[account(
         init, 
         seeds=[
@@ -202,37 +187,27 @@ pub struct SpawnMonster<'info> {
         space = std::mem::size_of::<Monster>() + 8)
     ]
     pub monster: Account<'info, Monster>,
-
     #[account(mut)]
     pub player: Signer<'info>,
-
     pub system_program: Program<'info, System>,
 }
 
 pub fn run_spawn_monster(ctx: Context<SpawnMonster>) -> Result<()> {
+    ctx.accounts.monster.player = ctx.accounts.player.key().clone();
+    ctx.accounts.monster.game = ctx.accounts.game.key().clone();
+    ctx.accounts.monster.hitpoints = 100;
 
-    {
-        ctx.accounts.monster.player = ctx.accounts.player.key().clone();
-        ctx.accounts.monster.game = ctx.accounts.game.key().clone();
-        ctx.accounts.monster.hitpoints = 100;
+    ctx.accounts.player_account.next_monster_index = ctx.accounts.player_account.next_monster_index.checked_add(1).unwrap();
+    msg!("Monster Spawned!");
 
-        msg!("Monster Spawned!");
-    }
-
-    {
-        ctx.accounts.player_account.next_monster_index = ctx.accounts.player_account.next_monster_index.checked_add(1).unwrap();
-    }
-
-    {   // Spend 5 lamports to spawn monster
-        let action_point_to_spend = 5;
-
-        spend_action_points(
-            action_point_to_spend, 
-            &mut ctx.accounts.player_account,
-            &ctx.accounts.player.to_account_info(), 
-            &ctx.accounts.system_program.to_account_info()
-        )?;
-    }
+    // Spend 5 lamports to spawn monster
+    let action_point_to_spend = 5;
+    spend_action_points(
+        action_point_to_spend, 
+        &mut ctx.accounts.player_account,
+        &ctx.accounts.player.to_account_info(), 
+        &ctx.accounts.system_program.to_account_info()
+    )?;
 
     Ok(())
 }
@@ -240,23 +215,19 @@ pub fn run_spawn_monster(ctx: Context<SpawnMonster>) -> Result<()> {
 // ----------- ATTACK MONSTER ----------
 #[derive(Accounts)]
 pub struct AttackMonster<'info> {
-
     #[account(
         mut,
         has_one = player,
     )]
     pub player_account: Box<Account<'info, Player>>,
-
     #[account(
         mut,
         has_one = player,
         constraint = monster.game == player_account.game
     )]
     pub monster: Box<Account<'info, Monster>>,
-
     #[account(mut)]
     pub player: Signer<'info>,
-
     pub system_program: Program<'info, System>,
 }
 
@@ -264,45 +235,40 @@ pub fn run_attack_monster(ctx: Context<AttackMonster>) -> Result<()> {
 
     let mut did_kill = false;
 
-    {
-        let hp_before_attack =  ctx.accounts.monster.hitpoints;
-        let hp_after_attack = ctx.accounts.monster.hitpoints.saturating_sub(1);
-        let damage_dealt = hp_before_attack - hp_after_attack;
-        ctx.accounts.monster.hitpoints = hp_after_attack;
+    let hp_before_attack =  ctx.accounts.monster.hitpoints;
+    let hp_after_attack = ctx.accounts.monster.hitpoints.saturating_sub(1);
+    let damage_dealt = hp_before_attack - hp_after_attack;
+    ctx.accounts.monster.hitpoints = hp_after_attack;
 
         
 
-        if hp_before_attack > 0 && hp_after_attack == 0 {
-            did_kill = true;
-        }
-
-        if  damage_dealt > 0 {
-            msg!("Damage Dealt: {}", damage_dealt);
-        } else {
-            msg!("Stop it's already dead!");
-        }
+    if hp_before_attack > 0 && hp_after_attack == 0 {
+        did_kill = true;
     }
 
-    {
-        ctx.accounts.player_account.experience = ctx.accounts.player_account.experience.saturating_add(1);
-        msg!("+1 EXP");
-
-        if did_kill {
-            ctx.accounts.player_account.kills = ctx.accounts.player_account.kills.saturating_add(1);
-            msg!("You killed the monster!");
-        }
+    if  damage_dealt > 0 {
+        msg!("Damage Dealt: {}", damage_dealt);
+    } else {
+        msg!("Stop it's already dead!");
     }
 
-    {   // Spend 1 lamports to attack monster
-        let action_point_to_spend = 1;
+    ctx.accounts.player_account.experience = ctx.accounts.player_account.experience.saturating_add(1);
+    msg!("+1 EXP");
 
-        spend_action_points(
-            action_point_to_spend, 
-            &mut ctx.accounts.player_account,
-            &ctx.accounts.player.to_account_info(), 
-            &ctx.accounts.system_program.to_account_info()
-        )?;
+    if did_kill {
+        ctx.accounts.player_account.kills = ctx.accounts.player_account.kills.saturating_add(1);
+        msg!("You killed the monster!");
     }
+
+    // Spend 1 lamports to attack monster
+    let action_point_to_spend = 1;
+
+    spend_action_points(
+        action_point_to_spend, 
+        &mut ctx.accounts.player_account,
+        &ctx.accounts.player.to_account_info(), 
+        &ctx.accounts.system_program.to_account_info()
+    )?;
 
     Ok(())
 }
@@ -310,23 +276,19 @@ pub fn run_attack_monster(ctx: Context<AttackMonster>) -> Result<()> {
 // ----------- REDEEM TO TREASURY ----------
 #[derive(Accounts)]
 pub struct CollectActionPoints<'info> {
-
     #[account(
         mut,
         has_one=treasury
     )]
     pub game: Box<Account<'info, Game>>,
-
     #[account(
         mut,
         has_one=game
     )]
     pub player: Box<Account<'info, Player>>,
-
     #[account(mut)]
     /// CHECK: It's being checked in the game account
     pub treasury: AccountInfo<'info>,
-
     pub system_program: Program<'info, System>,
 }
 
@@ -379,5 +341,4 @@ pub mod rpg {
         sol_log_compute_units();
         Ok(())
     }
-
 }
